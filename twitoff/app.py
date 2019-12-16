a358f6fde37e87787a5a12e9cb2e0b38ea525347
kryssyco <kjshepard70@hotmail.com>
2019-12-15 21:10:28 -0700

    Input code after installing gunicorn in a fresh pip environment

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,18 +1,40 @@
 '''Main Application for Twitoff'''
-from flask import Flask
-from .models import DB
-
-
-
+from decouple import config
+from flask import Flask, render_template, request
+from .models import DB, User
+from .twitter import add_or_update_user, update_all_users
 
 def create_app():
     """Create and configures an instance of the Flask application"""
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///twitoff_db.sqlite3'
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     DB.init_app(app)
 
     @app.route('/')
     def root():
-        return "Welcome to our app!"
+        users = User.query.all()
+        return render_template('base.html', title='Home', users=users)
+
+    @app.route('/user', methods=['POST', 'GET'])
+    @app.route('/user/<name>', methods=['GET'])
+    def user(name=None, message=''):
+        name = name or request.values['user_name']
+        try:
+            if request.method == 'POST':
+                add_or_update_user(name)
+                message = 'User {} successfully added!'.format(name)
+            tweets = User.query.filter(User.name == name).one().tweets
+        except Exception as e:
+            message = "Error adding or fetching {}: {}".format(name, e)
+            tweets = []
+        return render_template('user.html', title=name, tweets=tweets,
+                                message=message)
+
+    @app.route('/reset')
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return render_template('base.html', title='DB Reset', users=[])
     return app
